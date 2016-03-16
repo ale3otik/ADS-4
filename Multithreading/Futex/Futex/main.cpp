d2af3f808846dd2b4be705f0c29511c193164e77
Alex <aleksey.zotov@phystech.edu>
2016-03-17 01:36:51 +0300

    time tests mutex comare

--- Multithreading/Futex/Futex/main.cpp
+++ Multithreading/Futex/Futex/main.cpp
@@ -10,43 +10,70 @@
 #include <thread>
 #include <vector>
 #include <mutex>
+#include <cassert>
+#include <chrono>
 #include "futex.hpp"
 using std::vector;
 using std::thread;
-const int nthreads = 10;
-void check_lock(futex * f , int ind) {
-    f->lock();
-    std::cout << "I'm thread number " << ind << "\n";
-    f->unlock();
+using std::cout;
+using std::endl;
+
+template <class M>
+void inc(M * mutex, int * value , int * limit , int * thrcount) {
+    while(*value < *limit) {
+        mutex->lock();
+        if(*value < *limit) {
+            ++*value;
+            ++*thrcount;
+        }
+        mutex->unlock();
+    }
 }
 
-void check_try_lock(futex * f , int ind) {
-    if(f->try_lock()) {
-        std::cout << "I CAN !! " << ind << "\n";
-        f->unlock();
+template <class M>
+void check(int limit , int64_t nthreads) {
+    M mutex;
+    int value = 0;
+    int thrcount = 0;
+    vector<thread> threads;
+    for(int i = 0 ;  i < nthreads ;++ i) {
+        threads.push_back(thread(inc<M>, &mutex , &value , &limit , &thrcount));
     }
+
+    for(auto & x : threads) {
+        x.join();
+    }
+    assert(value == thrcount && value == limit);
 }
+
 int main() {
-    futex f;
-    vector<thread> th;
-    for(int i = 0 ;  i < nthreads ;++ i) {
-        th.push_back(thread(check_lock , &f , i));
+    int64_t result_time[4];
+    const int64_t limit = (int)1e7;
+    int64_t nthreads[] = {std::thread::hardware_concurrency() / 2 , std::thread::hardware_concurrency() * 2};
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    for(int i = 0; i < 2 ; ++i){
+        futex f;
+        start = std::chrono::system_clock::now();
+        check<futex>(limit, nthreads[i]);
+        end = std::chrono::system_clock::now();
+        result_time[i] = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
     }
     
-    for(auto & x : th) {
-        x.join();
+    for(int i = 2; i < 4 ; ++i){
+        std::mutex mtx;
+        start = std::chrono::system_clock::now();
+        check<std::mutex>(limit, nthreads[i%2]);
+        end = std::chrono::system_clock::now();
+        result_time[i] = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
     }
     
-    std::cout<< "_____________" << std::endl;
-    th.clear();
-    for(int i = 0 ;  i < nthreads ;++ i) {
-        th.push_back(thread(check_try_lock , &f , i));
+    cout << "hardware_concurrency : " << std::thread::hardware_concurrency() << "\n";
+    cout << "limit : " << limit << "\n\n";
+    
+    for(int i = 0; i < 4; ++i){
+        cout << (i < 2 ? "futex" : "mutex" ) <<" nthreads :" << nthreads[i%2] << " time : " << result_time[i] << "ms" << endl;
     }
     
-    for(auto & x : th) {
-        x.join();
-    }
-    std::cout << std::endl;
     return 0;
 }
 
